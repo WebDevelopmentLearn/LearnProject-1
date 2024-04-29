a6a0533357d5f659c5e85128b306998ef24f3761
007killer2 <savenko.artem@gmx.de>
2024-04-29 15:17:30 +0200

    Страница куки

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+const rejectBtn = document.querySelector("#reject_btn");
+const acceptBtn = document.querySelector("#accept_btn");
+const cookiesPage = document.querySelector(".coockie_page");
+rejectBtn.addEventListener("click", () => {
+  let cookiesPageClassList = cookiesPage.classList;
+  cookiesPageClassList.add("hidden");
+  console.log(`Reject All: Hide cookies`);
+});
+
+acceptBtn.addEventListener("click", () => {
+  let cookiesPageClassList = cookiesPage.classList;
+  cookiesPageClassList.add("hidden");
+  console.log(`Accept: Hide cookies`);
+});
