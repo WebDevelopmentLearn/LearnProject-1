52dbdf03701d34e6cbfcaa90f6a263535be41de0
007killer2 <savenko.artem@gmx.de>
2024-04-30 16:26:19 +0200

    Страница с видео

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,21 @@ acceptBtn.addEventListener("click", () => {
   cookiesPageClassList.add("hidden");
   console.log(`Accept: Hide cookies`);
 });
+
+const videoFrame = document.querySelector("#video_frame");
+const videosList = [
+  "IKXrzTmug_E?si=pSiYkRmy72fKecKM",
+  "QZWrJKalV1o?si=Gk8WQP3QFBetSaPr",
+  "vdtDyAevSU0?si=q2KLEBiv3ZTeD7bD",
+  "mnOrDCSr2XA?si=65uQY-ll6BGXl1fD",
+  "lJyxWQPHotw?si=kQOjx_02DO3A0ufA",
+];
+
+let currentVideo = Math.floor(videosList.length * Math.random());
+// console.log(
+//   `RandomValue: ${currentVideo}` + `\n` + `Link: ${videosList[currentVideo]}`
+// );
+videoFrame.setAttribute(
+  "src",
+  `https://www.youtube.com/embed/` + `${videosList[currentVideo]}`
+);
